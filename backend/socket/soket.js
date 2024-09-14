1f0196131758ce0ac2e24f6bd71b6b6be3439907
liuxin85 <liuxin85@gmail.com>
2024-09-14 19:49:22 +0800

    feat: add useListenMessages

--- backend/socket/soket.js
+++ backend/socket/soket.js
@@ -12,11 +12,27 @@ const io = new Server(server, {
   },
 });
 
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
+
+const userSocketMap = {}; // {userId: socketId}
+
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
 
+  const userId = socket.handshake.query.userId;
+
+  if (userId !== undefined) userSocketMap[userId] = socket.id;
+
+  // io.emit() is used to emit an event to all connected clients
+  io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
+  // socket.on() is used to listen to the event, can be used both on client and server side
   socket.on('disconect', () => {
     console.log('User disconnected', socket.id);
+    delete userSocketMap[userId];
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
   });
 });
 
